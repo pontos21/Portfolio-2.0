4b701f6b6615252ff76962f3f13a7d79baae0151
paul serafim <paul.serafim.3021@gmail.com>
2022-11-08 12:06:27 +1100

    project section "needs more work"

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 
 const Home: NextPage = () => {
@@ -33,6 +35,13 @@ const Home: NextPage = () => {
       <Skills />
     </section>
 
+    <section id="projects" className='snap-start'>
+      <Projects />
+    </section>
+
+    <section id="contact" className='snap-start'>
+      <ContactMe />
+    </section>
 
 
     </div>
